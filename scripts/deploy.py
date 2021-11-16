77b8f579dab9eaf898129d548faa7d6a694a2669
Atiqur Rahman <atiqur.india@gmail.com>
2021-11-16 14:43:51 +0530

    Use .env in Brownie

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,11 @@
 from brownie import accounts
+import os
 
 
 def deploy_simple_storage():
     # Uses the first account in the list of 10 accounts automatically created by brownie
     # account = accounts[0]
-    account = accounts.load("freecodecamp-account")
+    account = accounts.add(os.getenv("PRIVATE_KEY"))
     print(account)
 
 
